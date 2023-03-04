66e0c1aa908583742d3a43c2ede92d4b171d66d1
unknown <dimon-_@mail.ru>
2023-03-04 15:05:55 +0300

    correct

--- ip_print.cpp
+++ ip_print.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <tuple>
 #include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace print {
@@ -28,109 +29,22 @@ template <typename... Types>
 struct is_tuple<typename std::tuple<Types...>> : std::true_type {};
 
 template <typename T>
-void print_ip(
-    T& data,
-    typename std::enable_if<std::is_same<T, int8_t>::value>::type* = {}) {
-  auto dex = std::bitset<8>(data).to_ulong();
-  std::cout << std::dec << dex << std::endl;
-}
-
-template <typename T>
-void print_ip(
-    T& data,
-    typename std::enable_if<std::is_same<T, int16_t>::value>::type* = {}) {
-  std::string first_8, second_8;
-  auto to_str = std::bitset<16>(data).to_string();
-  for (size_t i = 0; i != to_str.size(); ++i) {
-    if (i < 8) {
-      first_8 += to_str[i];
-    }
-    if (i > 7) {
-      second_8 += to_str[i];
+void print_ip(T& data,
+              typename std::enable_if<
+                  std::is_integral<T>::value && !std::is_same<T, bool>::value &&
+                  !std::is_same<T, char>::value>::type* = {}) {
+  auto to_unchar = reinterpret_cast<unsigned char*>(&data);
+  auto n = 0;
+  for (auto i = sizeof(to_unchar) - 1; i != -1; --i) {
+    ++n;
+    if (std::bitset<8>(to_unchar[i]).to_ullong() != 204) {
+      std::cout << std::dec << std::bitset<8>(to_unchar[i]).to_ullong();
+      if (n != sizeof(to_unchar)) {
+        std::cout << ".";
+      }
     }
   }
-  auto res1 = std::bitset<8>(first_8).to_ulong();
-  auto res2 = std::bitset<8>(second_8).to_ulong();
-
-  std::cout << std::dec << res1 << "." << res2 << std::endl;
-}
-
-template <typename T>
-void print_ip(
-    T& data,
-    typename std::enable_if<std::is_same<T, int32_t>::value>::type* = {}) {
-  std::string first_8, second_8, third_8, thouth_8;
-
-  auto to_str = std::bitset<32>(data).to_string();
-  for (size_t i = 0; i != to_str.size(); ++i) {
-    if (i < 8) {
-      first_8 += to_str[i];
-    }
-    if (i > 7 && i < 16) {
-      second_8 += to_str[i];
-    }
-    if (i > 16 && i < 24) {
-      third_8 += to_str[i];
-    }
-    if (i > 23 && i < 32) {
-      thouth_8 += to_str[i];
-    }
-  }
-  auto res1 = std::bitset<8>(first_8).to_ulong();
-  auto res2 = std::bitset<8>(second_8).to_ullong();
-  auto res3 = std::bitset<8>(third_8).to_ulong();
-  auto res4 = std::bitset<8>(thouth_8).to_ulong();
-
-  std::cout << std::dec << res1 << "." << res2 << "." << res3 << "." << res4
-            << std::endl;
-}
-
-template <typename T>
-void print_ip(
-    T& data,
-    typename std::enable_if<std::is_same<T, int64_t>::value>::type* = {}) {
-  std::string first_8, second_8, third_8, thouth_8, thith_8, six_8, seven_8,
-      eight_8;
-
-  auto to_str = std::bitset<64>(data).to_string();
-  for (size_t i = 0; i != to_str.size(); ++i) {
-    if (i < 8) {
-      first_8 += to_str[i];
-    }
-    if (i > 7 && i < 16) {
-      second_8 += to_str[i];
-    }
-    if (i > 16 && i < 24) {
-      third_8 += to_str[i];
-    }
-    if (i > 23 && i < 32) {
-      thouth_8 += to_str[i];
-    }
-    if (i > 31 && i < 40) {
-      thith_8 += to_str[i];
-    }
-    if (i > 39 && i < 48) {
-      six_8 += to_str[i];
-    }
-    if (i > 47 && i < 56) {
-      seven_8 += to_str[i];
-    }
-    if (i > 55 && i < 64) {
-      eight_8 += to_str[i];
-    }
-  }
-  auto res1 = std::bitset<8>(first_8).to_ulong();
-  auto res2 = std::bitset<8>(second_8).to_ullong();
-  auto res3 = std::bitset<8>(third_8).to_ulong();
-  auto res4 = std::bitset<8>(thouth_8).to_ulong();
-  auto res5 = std::bitset<8>(thith_8).to_ulong();
-  auto res6 = std::bitset<8>(six_8).to_ulong();
-  auto res7 = std::bitset<8>(seven_8).to_ulong();
-  auto res8 = std::bitset<8>(eight_8).to_ulong();
-
-  std::cout << std::dec << res1 << "." << res2 << "." << res3 << "." << res4
-            << "." << res5 << "." << res6 << "." << res7 << "." << res8
-            << std::endl;
+  std::cout << std::endl;
 }
 
 template <typename T>
@@ -141,9 +55,10 @@ void print_ip(
 }
 
 template <typename T>
-void print_ip(T& container,
-              typename std::enable_if<is_vector<T>::value>::type* = {}) {
-  size_t n = 0;
+void print_ip(const T& container,
+              typename std::enable_if<is_vector<T>::value ||
+                                      is_list<T>::value>::type* = {}) {
+  auto n = 0;
   for (const auto& i : container) {
     std::cout << i;
     ++n;
@@ -155,25 +70,14 @@ void print_ip(T& container,
 }
 
 template <typename T>
-void print_ip(T& container,
-              typename std::enable_if<is_list<T>::value>::type* = {}) {
-  size_t n = 0;
-  for (const auto& i : container) {
-    std::cout << i;
-    ++n;
-    if (n != container.size()) {
-      std::cout << ".";
-    }
-  }
-  std::cout << std::endl;
-}
-
-template <typename T>
-void print_ip(T& container,
+void print_ip(const T& container,
               typename std::enable_if<is_tuple<T>::value>::type* = {}) {
-  std::cout << std::get<0>(container) << "." << std::get<1>(container) << "."
-            << std::get<2>(container) << "." << std::get<3>(container)
-            << std::endl;
+  std::apply(
+      [](auto&&... args) {
+        size_t n{0};
+        ((std::cout << args << (++n != std::tuple_size<T>{} ? "." : "")), ...);
+      },
+      container);
 }
 
 }  // namespace print
